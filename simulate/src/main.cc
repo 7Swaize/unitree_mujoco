7302aa8c9779719bb9b2354ad0da81c4dd69e189
7Swaize <rahejasachit@gmail.com>
2026-04-02 21:22:34 -0400

    settings added to .yaml

--- simulate/src/main.cc
+++ simulate/src/main.cc
@@ -672,7 +672,7 @@ int main(int argc, char **argv)
 
   // Load simulation configuration
   std::filesystem::path proj_dir = std::filesystem::path(getExecutableDir()).parent_path();
-  param::config.load_from_yaml(proj_dir / "config.yaml");
+  param::config.load_from_yaml(proj_dir / "src" / "config" / "main.yaml");
   param::helper(argc, argv);
   if(param::config.robot_scene.is_relative()) {
     param::config.robot_scene = proj_dir.parent_path() / "unitree_robots" / param::config.robot / param::config.robot_scene;

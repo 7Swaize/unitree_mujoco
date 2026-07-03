15a4af6d30b4680097e59d79bc66c6feaa91f4e9
7Swaize <rahejasachit@gmail.com>
2026-07-03 20:54:30 +0000

    Remove debug statements (#9)

--- simulate/src/python/go2sim/bridge/sport_bridge.py
+++ simulate/src/python/go2sim/bridge/sport_bridge.py
@@ -176,23 +176,15 @@ class SportBridge:
         
 
     def _handle_noargs_cmd(self, command: SportCommand, active_request: Optional[iox2.ActiveRequest]) -> None:
-        print(f"[HANDLE NOARGS] command={command} | thread={threading.get_ident()}")
-
         self._last_q = self._api_mappings[command].execute(self._last_q, self._adapter_stop_event)
         status = CommandStatus.INTERRUPTED if self._adapter_stop_event.is_set() else CommandStatus.OK
         self._respond(active_request, status)
 
-        print("[HANDLE NOARGS DONE]")
-
 
     def _handle_floatargs_cmd(self, command: SportCommand, active_request: Optional[iox2.ActiveRequest], arg1: float, arg2: float) -> None:
-        print(f"[HANDLE FLOATARGS] command={command} | thread={threading.get_ident()}")
-
         self._last_q = self._api_mappings[command].set_floatargs(arg1, arg2).execute(self._last_q, self._adapter_stop_event)
         status = CommandStatus.INTERRUPTED if self._adapter_stop_event.is_set() else CommandStatus.OK
         self._respond(active_request, status)
-
-        print("[HANDLE FLOATARGS DONE]")
 
 
     def _respond(self, active_request: Optional[iox2.ActiveRequest], status: CommandStatus) -> None:

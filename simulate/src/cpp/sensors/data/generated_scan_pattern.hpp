1bb61462cd97a34eda9e901290dbc5cb8073e4de
7Swaize <rahejasachit@gmail.com>
2026-07-09 18:40:18 +0000

    Remove generated files (#12)

--- simulate/src/cpp/sensors/data/generated_scan_pattern.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <array>
-#include <cstddef>
-
-namespace lidar_data {
-    inline constexpr std::size_t TotalElements = 2400000;
-    inline constexpr std::size_t TotalRows = 800000;
-    alignas(64) extern const std::array<float, 2400000> Mid360ScanPatternData;
-}

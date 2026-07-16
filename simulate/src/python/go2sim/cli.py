652a46b942f37177aebdcc3379f2a5a6def8897b
7Swaize <rahejasachit@gmail.com>
2026-07-16 00:41:39 +0000

    Fix hang v1.0.0 (#23)

--- simulate/src/python/go2sim/cli.py
+++ simulate/src/python/go2sim/cli.py
@@ -6,26 +6,15 @@ import importlib.resources as resources
 def get_cpp_binary():
     return str(resources.files("go2sim").joinpath("cpp/go2sim_cpp"))
 
-
-def _wait_or_kill(proc: subprocess.Popen, timeout: float = 5.0) -> None:
-    try:
-        proc.wait(timeout=timeout)
-    except subprocess.TimeoutExpired:
-        proc.kill()
-        proc.wait()
-
-
 def main():
     cpp_bin = get_cpp_binary()
 
     cpp = subprocess.Popen([cpp_bin])
-    py = subprocess.Popen([sys.executable,"-m", "go2sim.main"], start_new_session=True)
+    py = subprocess.Popen([sys.executable,"-m", "go2sim.main"])
 
     def cleanup(*_):
         py.terminate()
         cpp.terminate()
-
-        _wait_or_kill(py)
         sys.exit(0)
 
     signal.signal(signal.SIGINT, cleanup)

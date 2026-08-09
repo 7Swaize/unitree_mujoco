7b5c32c74734d209337e489df01d9b2d492cb2f7
7Swaize <rahejasachit@gmail.com>
2026-08-09 06:03:31 +0000

    Void BalanceStand | temp (#32)

--- simulate/src/python/go2sim/adapters/sport/balance_stand.py
+++ simulate/src/python/go2sim/adapters/sport/balance_stand.py
@@ -7,6 +7,4 @@ from ..adapter import Adapter
 class BalanceStand(Adapter):
     @override
     def execute(self, cancel_event: threading.Event) -> None:
-        self._policy_controller.set_move_cmd(0.0, 0.0, 0.0)
-        self._policy_controller.activate()
-        cancel_event.wait(timeout=0.05)
+        pass
